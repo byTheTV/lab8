c0084fe84d6dc8d9e39e703af926c40566a7989f
Tarasov Vladislav <467667@niuitmo.ru>
2025-05-22 15:44:48 +0300

    Почищены ненужные папки. Улучшена структура проекта

---
+++ src/main/frontend/types.d.ts
@@ -0,0 +1,7 @@
+declare module 'Frontend/generated/jar-resources/flow/generated-flow-imports.js' {
+    export * from './generated-flow-imports';
+}
+
+declare module 'Frontend/generated/jar-resources/flow/generated-flow-imports.d.ts' {
+    export * from './generated-flow-imports';
+} 
